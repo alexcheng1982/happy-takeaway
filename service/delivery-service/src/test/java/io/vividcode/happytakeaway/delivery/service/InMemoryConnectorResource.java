f4ccfc397613047ca8d39e28b445773b94010f0f
Fu Cheng <alexcheng1982@gmail.com>
2022-03-17 00:29:25 +1300

    fix test cases

--- service/delivery-service/src/test/java/io/vividcode/happytakeaway/delivery/service/InMemoryConnectorResource.java
+++ service/delivery-service/src/test/java/io/vividcode/happytakeaway/delivery/service/InMemoryConnectorResource.java
@@ -1,17 +1,19 @@
 package io.vividcode.happytakeaway.delivery.service;
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
-import io.smallrye.reactive.messaging.connectors.InMemoryConnector;
+import io.smallrye.reactive.messaging.providers.connectors.InMemoryConnector;
 import java.util.HashMap;
 import java.util.Map;
 
-public class InMemoryConnectorResource implements QuarkusTestResourceLifecycleManager {
+public class InMemoryConnectorResource implements
+    QuarkusTestResourceLifecycleManager {
 
   @Override
   public Map<String, String> start() {
     Map<String, String> env = new HashMap<>();
     Map<String, String> connector1 = InMemoryConnector
-        .switchIncomingChannelsToInMemory("delivery-pickup-invitation-accepted");
+        .switchIncomingChannelsToInMemory(
+            "delivery-pickup-invitation-accepted");
     Map<String, String> connector2 = InMemoryConnector
         .switchOutgoingChannelsToInMemory("delivery-pickup-invitation");
     env.putAll(connector1);
